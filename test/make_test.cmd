0a671f3396c41c9cefeddd50c94d3ab39cf8696b
Jon <dowlingjh@gmail.com>
2024-01-24 18:49:27 +0200

    Added tests

---
+++ test/make_test.cmd
@@ -0,0 +1,14 @@
+echo "Japanese: “Hello”" >こんにちは.txt
+echo "Russian: “Hello”" >Привет.txt
+echo "Chinese: “Hello”" >你好.txt
+echo "Korean: “Hello”" >안녕하세요.txt
+echo "Earth emojis" >🌍🌎🌏.txt
+echo "Computer-related emojis" >💻🖥️⌨️🖱️.txt
+echo "Fruit emojis" >🍎🍊🍌🍉🍇.txt
+echo "Animal emojis" >🐶🐱🐭🐹🐰.txt
+echo "Vehicle emojis" >🚗🚕🚙🚌🚎.txt
+echo "Sport emojis" >⚽🏀🏈⚾🎾.txt
+echo "Polish: “Hello”" >Cześć.txt
+echo "Hindi: “Hello”" >हैलो.txt
+echo "Hebrew: “Hello”" >שלום.txt
+echo "Arabic: “Hello”" >مرحبا.txt
